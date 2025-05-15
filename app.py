3a949cbffab3fae1474f540fcaaed09a0ccc841d
Vanellopie <144303329+Vanellopie@users.noreply.github.com>
2025-05-15 12:30:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def extract_features_for_all_images(valid_image_paths):
     return features_array, valid_filenames
 
 # --- Chatbot Configuration ---
-api_key = "AIzaSyC43kbqD_c3wYz3VVgSUwg0U4Hz8Bx5GjE"
+api_key = "AIzaSyCcbyzKP2EK4jEjdxRSf2Xc2V86ZvEGisU"
 brave_api_key = "BSAP1ZmJl9wMXKDvGnGM78r9__i_VuG"
 
 SYSTEM_PROMPT = """
